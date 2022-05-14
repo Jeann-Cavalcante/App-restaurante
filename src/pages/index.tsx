b23c1c3891b4fc06d98aae65facbbed25cc274cb
Jean <jeanncavalcante@gmail.com>
2022-05-14 02:21:36 -0300

    input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import logoImg from "../../public/logo.svg";
+import styles from "../../styles/Home.module.scss";
+import { Input } from "../components/ui/input";
 
 export default function Home() {
   return (
@@ -6,6 +11,16 @@ export default function Home() {
       <Head>
         <title>SujeitoPizza - Faça seu login</title>
       </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="logo" />
+
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu email" type="email" />
+            <Input placeholder="Digite sua senha" type="password" />
+          </form>
+        </div>
+      </div>
     </>
   );
 }

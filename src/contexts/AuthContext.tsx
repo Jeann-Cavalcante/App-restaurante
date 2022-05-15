a05ccf250262744536298bfcc07051d027f331a0
Jean <jeanncavalcante@gmail.com>
2022-05-14 23:32:13 -0300

    login

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useState } from "react";
-import { destroyCookie } from "nookies";
+import { destroyCookie, setCookie, parseCookies } from "nookies";
 import Router from "next/router";
+import { api } from "../services/apiClient";
 
 type AuthContextData = {
   user: UserProps;
@@ -39,7 +40,34 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>();
   const isAuthenticated = !!user; //!! transforma em boolean
   async function signIn({ email, password }: SingInProps) {
-    alert("Clicooou seniooor bom esse");
+    try {
+      const response = await api.post("/session", {
+        email,
+        password,
+      });
+      // console.log(response.data);
+
+      const { id, name, token } = response.data;
+
+      setCookie(undefined, "@nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // Expira em 1 Mês
+        path: "/", // Quais caminhos terão acesso ao cookie
+      });
+
+      setUser({
+        id,
+        name,
+        email,
+      });
+
+      //Passar para proxima requisiçoes o nosso token
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      //Redirecionar o user para /dashboard
+      Router.push("/dashboard");
+    } catch (err) {
+      console.log("Erro ao acessa", err);
+    }
   }
 
   return (

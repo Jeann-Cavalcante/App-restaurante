225b6e541b3c5f0ab240b35efbed6809a23b1c40
Jean <jeanncavalcante@gmail.com>
2022-05-25 22:07:20 -0300

    modal

--- src/components/ModalOrder/index.tsx
+++ src/components/ModalOrder/index.tsx
@@ -1,7 +1,55 @@
-export function ModalOrder() {
+import { FiX } from "react-icons/fi";
+import Modal from "react-modal";
+
+import { OrderItemProps } from "../../pages/dashboard";
+import styles from "./styles.module.scss";
+
+interface ModalOrderProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+  order: OrderItemProps[];
+}
+
+export function ModalOrder({ isOpen, onRequestClose, order }: ModalOrderProps) {
+  const customStyles = {
+    content: {
+      top: "50%",
+      bottom: "auto",
+      left: "50%",
+      right: "auto",
+      padding: "30px",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "#1d1d2e",
+    },
+  };
   return (
-    <div>
-      <h1>Detalhes do pedido</h1>
-    </div>
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose} style={customStyles}>
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+        style={{ background: "transparent", border: 0 }}
+      >
+        <FiX size={45} color="#f34748" />;
+      </button>
+
+      <div className={styles.container}>
+        <h2>Detalhes do pedido</h2>
+        <span className={styles.table}>
+          <strong>mesa:{order[0].order.table}</strong>
+        </span>
+
+        {order.map((item) => (
+          <section key={item.id} className={styles.containerItem}>
+            <span>
+              {item.amount} - <strong>{item.product.name}</strong>
+            </span>
+            <span className={styles.description}>
+              {item.product.description}
+            </span>
+          </section>
+        ))}
+      </div>
+    </Modal>
   );
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -94,7 +94,13 @@ export default function Dashboard({ orders }: HomeProps) {
           </article>
         </main>
 
-        {modalVisible && <ModalOrder />}
+        {modalVisible && (
+          <ModalOrder
+            isOpen={modalVisible}
+            onRequestClose={handleCloseModal}
+            order={modalItem}
+          />
+        )}
       </div>
     </>
   );
